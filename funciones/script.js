600a3a0aa3319ca2bfbd1c7ddc95c68bd889e4dc
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2023-12-08 14:33:54 +0100

    Terminamos las funciones y realizamos 3 practicas

--- funciones/script.js
+++ funciones/script.js
@@ -136,8 +136,159 @@ if (hora >= 0 && hora <= 7){
         //No tiene la edad para pasar
         document.write("Espera chaval, no puedes pasar.");
     }
-
 }else{
     //Esta cerrada
     document.write("La discoteca esta cerrada.");
 }
+
+document.write("<br>");
+document.write("<br>");
+document.write("<b>Práctica 2: </b>");
+document.write("<br>");
+document.write("<br>");
+
+/*
+*                               PRACTICA 2
+*
+*   Este joven volvió a clase el dia siguiente de la fiesta, en su curso son un total de 19 alumnos. En su universidad ocurrio un problema
+*   y es que se rompió el sistema de registro de asistencias y durante los proximos 30dias no se pueden hacer registros de datos,
+*   las clases no podran comenzar hasta que alguien resuelva este problema....
+*   
+*   - Crear un minisistema que nos permita registrar a los alumnos presentes (p) y a los ausentes (a).
+*   - Pasados los 30 días mostrar la situacion final de todos los alumnos (nº total de ausentes y presentes).
+*   - Se puede tener un maximo de 10% de ausencias por semestre, si algun alumno tiene mas será suspenso.
+*/
+
+let cantidad = prompt("¿Cuantos alumnos son?");
+let alumnosTotales = [];
+
+for (let i = 0; i < cantidad; i++) {
+    alumnosTotales[i] = [prompt("Nombre del alumno " + (i+1)),0];
+}
+
+const tomarAsistencia = (nombre,p)=>{
+    let presencia = prompt(nombre);
+    if (presencia == "p" || presencia =="P") {
+        alumnosTotales[p][1]++;
+    }
+}
+
+for (i=0; i <= 30; i++){
+    for(alumno in alumnosTotales) {
+        tomarAsistencia(alumnosTotales[alumno][0], alumno);
+    }
+}
+
+for(alumno in alumnosTotales) {
+    let resultado = `${alumnosTotales[alumno][0]}:<br>
+    _________Presentes: ${alumnosTotales[alumno][1]}
+    _________Ausencias: ${30 - alumnosTotales[alumno][1]}`;
+
+    if (30 - alumnosTotales[alumno][1] > 17) {
+        resultado += "<b style='color:red'> REPROBADO POR FALTAR A CLASE</b><br><br>";
+    }else{
+        resultado += "<br><br>"
+    }
+    document.write(resultado);
+
+}
+
+/*
+*   Práctica 3 Crear una calculadora
+*/
+
+//Funciones de la calculadora
+function suma(num1, num2) {
+    //Transformamos a int los datos
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    //Creamos la variable y los sumamos
+    let resultado = num1 + num2;
+
+    //Devolvemos el resultado
+    return resultado;
+}
+
+//Funciones de la calculadora
+function resta(num1, num2) {
+    //Transformamos a int los datos
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    //Creamos la variable y los sumamos
+    let resultado = num1 - num2;
+
+    //Devolvemos el resultado
+    return resultado;
+}
+
+//Funciones de la calculadora
+function multiplicar(num1, num2) {
+    //Transformamos a int los datos
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    //Creamos la variable y los sumamos
+    let resultado = num1 * num2;
+
+    //Devolvemos el resultado
+    return resultado;
+}
+
+//Funciones de la calculadora
+function dividir(num1, num2) {
+    //Transformamos a int los datos
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    //Creamos la variable y los sumamos
+    let resultado = num1 / num2;
+
+    //Devolvemos el resultado
+    return resultado;
+}
+
+
+//Preguntamos al usuario que funcion quiere realizar
+let operacion = prompt("¿Que operacion deseas realizar? (1 = Sumar - 2 = Restar - 3 = Multiplicar - 4 = Dividir)")
+
+//Creamos las condiciones del programa para que, segun lo que haya escrito el usuario, realice una funcion u otra
+if (operacion == 1) {
+    //Pedimos los numeros al usuario
+    num1 = prompt("Dame el primer numero");
+    num2 = prompt("Dame el segundo numero");
+
+    //Mostramos el resultado por pantalla
+    document.write("El resultado de sumar los numeros " + num1 + " y " + num2 +" es: " + suma(num1,num2));
+
+}
+else if (operacion == 2){
+    //Pedimos los numeros al usuario
+    num1 = prompt("Dame el primer numero");
+    num2 = prompt("Dame el segundo numero");
+
+    //Mostramos el resultado por pantalla
+    document.write("El resultado de restar los numeros " + num1 + " y " + num2 +" es: " + resta(num1,num2));
+}
+else if (operacion == 3){
+        //Pedimos los numeros al usuario
+        num1 = prompt("Dame el primer numero");
+        num2 = prompt("Dame el segundo numero");
+    
+        //Mostramos el resultado por pantalla
+        document.write("El resultado de multiplicar los numeros " + num1 + " y " + num2 +" es: " + multiplicar(num1,num2));
+}
+else if (operacion == 4){
+        //Pedimos los numeros al usuario
+        num1 = prompt("Dame el primer numero");
+        num2 = prompt("Dame el segundo numero");
+    
+        //Mostramos el resultado por pantalla
+        document.write("El resultado de dividir los numeros " + num1 + " y " + num2 +" es: " + dividir(num1,num2));
+}
+else{
+    alert("Ha ocurrido un error el programa debe cerrarse.")
+}
+
+
