fed47bd4bc6e30c377a4ca5c66dd24b37bc75fa8
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-02-20 19:54:43 +0100

    Fechas actualizado

---
+++ Fechas/script.js
@@ -0,0 +1,21 @@
+'use stricts'
+
+var fecha = new Date();
+
+var year = fecha.getFullYear();
+var mes = fecha.getMonth();
+var dia = fecha.getDate()
+var hora = fecha.getHours();
+var min = fecha.getMinutes();
+var seg = fecha.getSeconds();
+
+var textoHora = `
+    El año es: ${year}
+    El mes es: ${mes+1}
+    El dia es: ${dia}
+    La hora es: ${hora}
+    Los minutos son: ${min}
+    Los segundos son: ${seg}
+`;
+
+console.log(textoHora);
