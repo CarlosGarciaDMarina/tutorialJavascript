87bd68842fa8125d015de9b14152f4ecacc81431
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-02-21 12:10:52 +0100

    Reorganizacion de carpetas

---
+++ Tutorial-jQuery/Libreria-jQuery/jQ/01-selectores.js
@@ -0,0 +1,13 @@
+
+//Comrpobamos si el documento esta cargado
+$(document).ready(function(){
+    console.log("Documento cargado !!")
+});
+
+//
+$(document).ready(function() {
+   
+    //Para seleccionar una id
+    $("#rojo")
+
+});
