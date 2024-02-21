f1a838624d487323cceba378794b669021786d8f
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-02-21 13:01:31 +0100

    Empezamos con los Selectores

--- Tutorial-jQuery/Libreria-jQuery/jQ/01-selectores.js
+++ Tutorial-jQuery/Libreria-jQuery/jQ/01-selectores.js
@@ -1,13 +1,38 @@
-
-//Comrpobamos si el documento esta cargado
+// Comrpobamos si el documento esta cargado
 $(document).ready(function(){
     console.log("Documento cargado !!")
 });
 
-//
-$(document).ready(function() {
-   
-    //Para seleccionar una id
-    $("#rojo")
 
+$(document).ready(function() {
+
+    // Selectores de ID
+    var rojo = $("#rojo").css("background", "red")
+                         .css("color", "white")
+                         .css("font-size", "30px");
+    var amarillo = $("#amarillo").css("background", "yellow")
+                                 .css("color", "green")
+                                 .css("font-size", "30px");
+    var verde = $("#verde").css("background", "green")
+                           .css("color", "white")
+                           .css("font-size", "30px");
+    var blamco = $("#blanco").css("font-size", "30px");
+
+    // Selector de CLASES
+    var mi_clase = $('.zebra').css("padding","5px");
+
+    // Llamamos al evento click asi
+    $('.sin_borde').click(function(){
+        console.log("Click");
+        // El this es para decirle que es para este elemento, y el addClass es para añadirle una clase al elemento
+        $(this).addClass('zebra');
+    });
+    /**
+     * Para seleccionar los elementos individuales que tienen las clases podemos hacerlo con un array o con eq. Ejmplo:
+     * Con un array - mi_clase[0]
+     * No es tan recomendable porque no puedes trabajar igual de funcioanl y sencillo como con eq
+     * 
+     * Con eq - mi_clase.eq(1)
+     * con el metodo eq podemos hacer mas cosas ya que tenemos los elementos de jQuery para poder trabajar
+     */
 });
