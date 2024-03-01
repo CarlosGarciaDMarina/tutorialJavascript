f9b3e5d4173d134d112f7dc2edce957c6469d84e
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-03-01 15:48:12 +0100

    Continuamos con directivas

--- TutorialAngular/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ TutorialAngular/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -9,10 +9,13 @@ export class ZapatillasComponent implements OnInit {
 
     public titulo: string = "Componente de Zapatillas";
     public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
 
     constructor(){
+        this.marcas = new Array;
         this.zapatillas = [
             new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80, true),
+            new Zapatilla('Reebook Spartan', 'Reebook', 'Gris', 145, false),
             new Zapatilla('Nike Airmax', 'Nike', 'Rojas', 190, true),
             new Zapatilla('Nike Runner MD', 'Nike', 'Negras', 70, true),
             new Zapatilla('Adidas Yezzy', 'Adidas', 'Gris', 180, false)
@@ -21,7 +24,18 @@ export class ZapatillasComponent implements OnInit {
 
     ngOnInit(): void {
         console.log(this.zapatillas);
+        this.getMarcas();
     }
 
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) =>{
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+
+            }
+        });
+
+        console.log(this.marcas);
+    }
 
 }
