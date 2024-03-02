f19e606c0d678e2e0112e394ff796f7b4e7f636f
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-03-02 12:56:39 +0100

    Páginas y Routing en Angular

---
+++ TutorialAngular/aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+// Importar los modulos del router de Angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+// Importar componentes
+import { HomeComponent } from './home/home.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+
+// Array de rutas
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent}, // Ruta home
+    {path: 'home', component: HomeComponent}, // Ruta home
+    {path: 'zapatillas', component: ZapatillasComponent}, // Ruta zapatillas
+    {path: 'videojuego', component: VideojuegoComponent}, // Ruta videojuegos
+    {path: 'cursos', component: CursosComponent}, // Ruta cursos
+    {path: 'cursos/:nombre', component: CursosComponent}, // Ruta cursos con opcion a recoger parametos
+    {path: 'cursos/:nombre/:followers', component: CursosComponent}, // Ruta cursos con opcion a recoger parametos
+    {path: '**', component: HomeComponent} // Ruta 404, se define la última de todas porque si no se encuentra ninguno de nuestros componentes, esta va a ser la que cargue
+];
+
+// Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
--- TutorialAngular/aprendiendo-angular/src/app/cursos/cursos.component.ts
+++ TutorialAngular/aprendiendo-angular/src/app/cursos/cursos.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'cursos',
   templateUrl: './cursos.component.html',
-  styleUrl: './cursos.component.css'
+  styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
-  
-  constructor(){
-    
-  }
+
+    public nombre!: string; // el simbolo de exclamacion indica que esta propiedad se inicializará más tarde
+    public followers!: number;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ){ }
   
   ngOnInit(): void {
-      
+       this._route.params.subscribe((params: Params) => {
+        this.nombre = params['nombre']; // Accedemos a la propiedad con ['nombre'] porque proviene de una firma de indice y asi evitamos errores haciendo el codigo mas robusto
+        this.followers = +params['followers']; // El símbolo + indica que el paramtero va a ser tipo number
+
+        /* EJEMPLO DE COMO Redirigir a paginas segun datos del navegador
+        if (this.nombre == 'ninguno') {
+          this._router.navigate(['/home']);
+
+        }
+        */
+       });
+  }
+  redirigir(){
+    this._router.navigate(['/zapatillas']);
   }
 
 }
